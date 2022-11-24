0df30b38b1ecf847524ade938bda52de43d35ea8
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2022-11-24 14:26:49 -0500

    ref-filter: add new "signature" atom
    This commit duplicates the code for `signature` atom from pretty.c
    to ref-filter.c. This feature will help to get rid of current duplicate
    implementation of `signature` atom when unifying implementations by
    using ref-filter logic everywhere when ref-filter can do everything
    pretty is doing.
    
    Add "signature" atom with `grade`, `signer`, `key`,
    `fingerprint`, `primarykeyfingerprint`, `trustlevel` as arguments.
    This code and its documentation are inspired by how the %GG, %G?,
    %GS, %GK, %GF, %GP, and %GT pretty formats were implemented.
    
    Co-authored-by: Hariom Verma <hariom18599@gmail.com>
    Co-authored-by: Jaydeep Das <jaydeepjd.8914@gmail.com>
    Mentored-by: Christian Couder <chriscool@tuxfamily.org>
    Mentored-by: Hariom Verma <hariom18599@gmail.com>
    Signed-off-by: Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
    Signed-off-by: Kousik Sanagavarapu <five231003@gmail.com>

--- t/lib-gpg.sh
+++ t/lib-gpg.sh
@@ -51,6 +51,27 @@ test_lazy_prereq GPG '
 	esac
 '
 
+test_lazy_prereq GPG2 '
+	gpg_version=$(gpg --version 2>&1)
+	test $? != 127 || exit 1
+
+	case "$gpg_version" in
+	"gpg (GnuPG) 1."*)
+		say "Skipping. For gpg v1.*, use prereq GPG"
+		! test_have_prereq GPG2
+		;;
+	*)
+		(gpgconf --kill all || : ) &&
+		gpg --homedir "${GNUPGHOME}" --import \
+			"$TEST_DIRECTORY"/lib-gpg/keyring.gpg &&
+		gpg --homedir "${GNUPGHOME}" --import-ownertrust \
+			"$TEST_DIRECTORY"/lib-gpg/ownertrust &&
+		gpg --homedir "${GNUPGHOME}" </dev/null >/dev/null \
+			--sign -u committer@example.com
+		;;
+	esac
+'
+
 test_lazy_prereq GPGSM '
 	test_have_prereq GPG &&
 	# Available key info:
