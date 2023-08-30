81b875679377283ab242ef1b4740c6e5f49005d3
Kousik Sanagavarapu <five231003@gmail.com>
2023-08-30 11:48:59 +0530

    ref-filter: sort numerically when ":size" is used
    Atoms like "raw" and "contents" have a ":size" option which can be used
    to know the size of the data. Since these atoms have the cmp_type
    FIELD_STR, they are sorted alphabetically from 'a' to 'z' and '0' to
    '9'. Meaning, even when the ":size" option is used and what we
    ultimatlely have is numbers, we still sort alphabetically.
    
    For example, consider the the following case in a repo
    
    refname			contents:size		raw:size
    =======			=============		========
    refs/heads/branch1	1130			1210
    refs/heads/master	300			410
    refs/tags/v1.0		140			260
    
    Sorting with "--format="%(refname) %(contents:size) --sort=contents:size"
    would give
    
    refs/heads/branch1 1130
    refs/tags/v1.0.0 140
    refs/heads/master 300
    
    which is an alphabetic sort, while what one might really expect is
    
    refs/tags/v1.0.0 140
    refs/heads/master 300
    refs/heads/branch1 1130
    
    which is a numeric sort (that is, a "$ sort -n file" as opposed to a
    "$ sort file", where "file" contains only the "contents:size" or
    "raw:size" info, each of which is on a newline).
    
    Same is the case with "--sort=raw:size".
    
    So, sort numerically whenever the sort is done with "contents:size" or
    "raw:size" and do it the normal alphabetic way when "contents" or "raw"
    are used with some other option (they are FIELD_STR anyways).
    
    Signed-off-by: Kousik Sanagavarapu <five231003@gmail.com>

--- t/t6300-for-each-ref.sh
+++ t/t6300-for-each-ref.sh
@@ -1017,16 +1017,16 @@ test_expect_success 'Verify sorts with raw' '
 test_expect_success 'Verify sorts with raw:size' '
 	cat >expected <<-EOF &&
 	refs/myblobs/blob8
-	refs/myblobs/first
 	refs/myblobs/blob7
-	refs/heads/main
 	refs/myblobs/blob4
 	refs/myblobs/blob1
 	refs/myblobs/blob2
 	refs/myblobs/blob3
 	refs/myblobs/blob5
 	refs/myblobs/blob6
+	refs/myblobs/first
 	refs/mytrees/first
+	refs/heads/main
 	EOF
 	git for-each-ref --format="%(refname)" --sort=raw:size \
 		refs/heads/main refs/myblobs/ refs/mytrees/first >actual &&
@@ -1136,6 +1136,29 @@ test_expect_success 'for-each-ref --format compare with cat-file --batch' '
 	git for-each-ref --format="%(objectname) %(objecttype) %(objectsize)
 %(raw)" refs/mytrees/first >actual &&
 	test_cmp expected actual
+'
+
+test_expect_success 'verify sorts with contents:size' '
+	cat >expect <<-\EOF &&
+	refs/heads/main
+	refs/tags/bogo
+	refs/tags/two
+	refs/tags/third
+	refs/heads/newtag
+	refs/heads/ambiguous
+	refs/tags/ambiguous
+	refs/tags/taggerless
+	refs/tags/main
+	refs/tags/testtag
+	refs/tags/subject-body
+	refs/tags/multiline
+	refs/tags/signed-empty
+	refs/tags/signed-short
+	refs/tags/signed-long
+	EOF
+	git for-each-ref --format="%(refname)" \
+		--sort=contents:size refs/heads/ refs/tags/ >actual &&
+	test_cmp expect actual
 '
 
 test_expect_success 'set up multiple-sort tags' '
