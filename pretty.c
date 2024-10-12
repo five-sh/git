ebdac83286b6ef338a5b4c8cc3b4f8df4a751bf6
Kousik Sanagavarapu <five231003@gmail.com>
2024-10-12 12:56:55 +0530

    pretty: introduce match_format_arg_value() and match_format_bool_arg()
    f46094a (ref-filter: add multiple-option parsing functions, 2023-07-24)
    introduced the match_atom_*() functions in ref-filter, which mimicked
    what match_placeholder_*() functions did, but for ref-filter atoms.  This
    was done because of the nature of input given to these functions -
    
      match_placeholder*() expect an input string which terminates with ')'
            so something like "abbrev=10,tags)"
    
      match_atom*() expect an input string which terminates with NUL
            so something like "abbrev=10,tags"
    
      when the given format to both pretty and ref-filter (ie say through
      log and for-each-ref respectively) is "%(describe:abbrev=10,tags)".
    
    Therefore, refactor match_placeholder_arg_value() so that it can handle
    being given any terminating character in its "to_parse".
    
    In addition to this refactoring, drop the NULL check for "valuestart"
    within the implementation of "match_placheholder_arg_value()" since it
    is useless, as callers already pass a non NULL "valuestart" and requiring
    them to pass a non NULL "valuestart" is a simple enough requirement.
    
    If the caller is sure that they don't have a value then they are
    better off using skip_prefix() instead of this function.
    
    While at it, rename it to "match_format_arg_value()" addressing its
    generality and rename "match_placeholder_bool_arg()" to
    "match_format_bool_arg()" respectively.
    
    The following commit will move this function to ref-filter and make it
    globally visible.
    
    Signed-off-by: Kousik Sanagavarapu <five231003@gmail.com>

--- pretty.c
+++ pretty.c
@@ -1192,46 +1192,56 @@ static size_t parse_padding_placeholder(const char *placeholder,
 	return 0;
 }
 
-static int match_placeholder_arg_value(const char *to_parse, const char *candidate,
-				       const char **end, const char **valuestart,
-				       size_t *valuelen)
+static int match_format_arg_value(const char *to_parse, const char *candidate,
+				  const char **end, const char **valuestart,
+				  size_t *valuelen, char term_char)
 {
 	const char *p;
 
-	if (!(skip_prefix(to_parse, candidate, &p)))
-		return 0;
-	if (valuestart) {
-		if (*p == '=') {
-			*valuestart = p + 1;
-			*valuelen = strcspn(*valuestart, ",)");
-			p = *valuestart + *valuelen;
-		} else {
-			if (*p != ',' && *p != ')')
-				return 0;
-			*valuestart = NULL;
-			*valuelen = 0;
+	if (!skip_prefix(to_parse, candidate, &p))
+		return 0; /* definitely not "candidate" */
+
+	if (*p == '=') {
+		/* we just saw "candidate=" */
+		*valuestart = p + 1;
+
+		p = strchr(*valuestart, ',');
+		if (!p) {
+			p = strchr(*valuestart, term_char);
+			if (!p)
+				BUG("%c is expected to terminate %s",
+				    term_char, to_parse);
 		}
+
+		*valuelen = p - *valuestart;
+	} else if (*p != ',' && *p != term_char) {
+		/* key begins with "candidate" but has more chars not term_char */
+		return 0;
+	} else {
+		/* just "candidate" without "=val" */
+		*valuestart = NULL;
+		*valuelen = 0;
 	}
-	if (*p == ',') {
+
+	/* p points at either the ',' or term_char after this key[=val] */
+	if (*p == ',')
 		*end = p + 1;
-		return 1;
-	}
-	if (*p == ')') {
+	else if (*p == term_char)
 		*end = p;
-		return 1;
-	}
-	return 0;
+
+	return 1;
 }
 
-static int match_placeholder_bool_arg(const char *to_parse, const char *candidate,
-				      const char **end, int *val)
+static int match_format_bool_arg(const char *to_parse, const char *candidate,
+				 const char **end, int *val, char term_char)
 {
 	const char *argval;
 	char *strval;
 	size_t arglen;
 	int v;
 
-	if (!match_placeholder_arg_value(to_parse, candidate, end, &argval, &arglen))
+	if (!match_format_arg_value(to_parse, candidate, end, &argval,
+				    &arglen, term_char))
 		return 0;
 
 	if (!argval) {
@@ -1299,7 +1309,7 @@ int format_set_trailers_options(struct process_trailer_options *opts,
 		if (**arg == ')')
 			break;
 
-		if (match_placeholder_arg_value(*arg, "key", arg, &argval, &arglen)) {
+		if (match_format_arg_value(*arg, "key", arg, &argval, &arglen, ')')) {
 			uintptr_t len = arglen;
 
 			if (!argval)
@@ -1312,14 +1322,14 @@ int format_set_trailers_options(struct process_trailer_options *opts,
 			opts->filter = format_trailer_match_cb;
 			opts->filter_data = filter_list;
 			opts->only_trailers = 1;
-		} else if (match_placeholder_arg_value(*arg, "separator", arg, &argval, &arglen)) {
+		} else if (match_format_arg_value(*arg, "separator", arg, &argval, &arglen, ')')) {
 			opts->separator = expand_string_arg(sepbuf, argval, arglen);
-		} else if (match_placeholder_arg_value(*arg, "key_value_separator", arg, &argval, &arglen)) {
+		} else if (match_format_arg_value(*arg, "key_value_separator", arg, &argval, &arglen, ')')) {
 			opts->key_value_separator = expand_string_arg(kvsepbuf, argval, arglen);
-		} else if (!match_placeholder_bool_arg(*arg, "only", arg, &opts->only_trailers) &&
-			   !match_placeholder_bool_arg(*arg, "unfold", arg, &opts->unfold) &&
-			   !match_placeholder_bool_arg(*arg, "keyonly", arg, &opts->key_only) &&
-			   !match_placeholder_bool_arg(*arg, "valueonly", arg, &opts->value_only)) {
+		} else if (!match_format_bool_arg(*arg, "only", arg, &opts->only_trailers, ')') &&
+			   !match_format_bool_arg(*arg, "unfold", arg, &opts->unfold, ')') &&
+			   !match_format_bool_arg(*arg, "keyonly", arg, &opts->key_only, ')') &&
+			   !match_format_bool_arg(*arg, "valueonly", arg, &opts->value_only, ')')) {
 			if (invalid_arg) {
 				size_t len = strcspn(*arg, ",)");
 				*invalid_arg = xstrndup(*arg, len);
@@ -1357,7 +1367,7 @@ static size_t parse_describe_args(const char *start, struct strvec *args)
 		for (i = 0; !found && i < ARRAY_SIZE(option); i++) {
 			switch (option[i].type) {
 			case DESCRIBE_ARG_BOOL:
-				if (match_placeholder_bool_arg(arg, option[i].name, &arg, &optval)) {
+				if (match_format_bool_arg(arg, option[i].name, &arg, &optval, ')')) {
 					if (optval)
 						strvec_pushf(args, "--%s", option[i].name);
 					else
@@ -1366,8 +1376,8 @@ static size_t parse_describe_args(const char *start, struct strvec *args)
 				}
 				break;
 			case DESCRIBE_ARG_INTEGER:
-				if (match_placeholder_arg_value(arg, option[i].name, &arg,
-								&argval, &arglen)) {
+				if (match_format_arg_value(arg, option[i].name, &arg,
+								&argval, &arglen, ')')) {
 					char *endptr;
 					if (!arglen)
 						return 0;
@@ -1379,8 +1389,8 @@ static size_t parse_describe_args(const char *start, struct strvec *args)
 				}
 				break;
 			case DESCRIBE_ARG_STRING:
-				if (match_placeholder_arg_value(arg, option[i].name, &arg,
-								&argval, &arglen)) {
+				if (match_format_arg_value(arg, option[i].name, &arg,
+								&argval, &arglen, ')')) {
 					if (!arglen)
 						return 0;
 					strvec_pushf(args, "--%s=%.*s", option[i].name, (int)arglen, argval);
@@ -1404,7 +1414,7 @@ static int parse_decoration_option(const char **arg,
 	const char *argval;
 	size_t arglen;
 
-	if (match_placeholder_arg_value(*arg, name, arg, &argval, &arglen)) {
+	if (match_format_arg_value(*arg, name, arg, &argval, &arglen, ')')) {
 		struct strbuf sb = STRBUF_INIT;
 
 		expand_string_arg(&sb, argval, arglen);
